c7b8fea9fcd4e7ab5134d20f86587e2999ee816e
Andrei Korikov <kpanic666@gmail.com>
2021-10-21 19:24:17 +0500

    Add filter and credits buttons

--- WhitehousePetition/ViewController.swift
+++ WhitehousePetition/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var filter: String?
+    var filteredPetitions: [Petition] {
+        if let filter = filter {
+            if !filter.isEmpty {
+                return petitions.filter { $0.title.contains(filter) }
+            }
+        }
+        
+        return petitions
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(promptForSearch))
         
         let urlString: String
         if navigationController?.tabBarItem.tag == 0 {
@@ -27,7 +40,7 @@ class ViewController: UITableViewController {
             }
         }
         
-        showError()
+        showMessage(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again.")
     }
     
     func parse(json: Data) {
@@ -37,18 +50,35 @@ class ViewController: UITableViewController {
         }
     }
     
-    func showError() {
-        let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+    func showMessage(title: String, message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func showCredits() {
+        showMessage(title: "Credits", message: "Data comes from the 'We The People API' of the Whitehouse")
+    }
+    
+    @objc func promptForSearch() {
+        let ac = UIAlertController(title: "Filter", message: "Enter the string to filter petitions by Title.", preferredStyle: .alert)
+        ac.addTextField()
+        
+        let filterAction = UIAlertAction(title: "OK", style: .default) { [unowned ac, weak self] _ in
+            self?.filter = ac.textFields?[0].text
+            self?.tableView.reloadData()
+        }
+        ac.addAction(filterAction)
+        
         present(ac, animated: true)
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        petitions.count
+        filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = petition.title
@@ -58,7 +88,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 }
